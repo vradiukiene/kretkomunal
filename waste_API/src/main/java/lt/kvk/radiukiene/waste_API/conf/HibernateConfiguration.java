23ae80c5a1a9dca6d7c931ea910ee15b9ed616de
vradiukiene <39836393+vradiukiene@users.noreply.github.com>
2019-02-25 22:07:05 +0200

    update HibernateConfiguration.java

--- waste_API/src/main/java/lt/kvk/radiukiene/waste_API/conf/HibernateConfiguration.java
+++ waste_API/src/main/java/lt/kvk/radiukiene/waste_API/conf/HibernateConfiguration.java
@@ -29,11 +29,38 @@ public class HibernateConfiguration {
 	private Environment environment;
 	
 	@Value("com.mysql.jdbc.Driver") String driverClassName;
-    @Value("jdbc:mysql://${DBHOSTNAME}:3306/${DBSCHEMA}") String url;
-    @Value("${DBUSERNAME}") String username;
-    @Value("${DBPASSWORD}") String password;
-    @Bean(name = "dataSource")
-    public DataSource getDataSource() {
+    	@Value("jdbc:mysql://${DBHOSTNAME}:3306/${DBSCHEMA}") String url;
+    	@Value("${DBUSERNAME}") String username;
+    	@Value("${DBPASSWORD}") String password;
+    	@Bean(name = "dataSource")
+	public ComboPooledDataSource getDataSource() {
+        	ComboPooledDataSource cpds = new ComboPooledDataSource();
+        	cpds.setJdbcUrl(url);
+       		cpds.setUser(username);
+        	cpds.setPassword(password);
+
+        	cpds.setInitialPoolSize(2);
+        	cpds.setMinPoolSize(2);
+        	cpds.setAcquireIncrement(2);
+        	cpds.setMaxPoolSize(10);
+        	cpds.setMaxStatements(100);
+        
+        	cpds.setTestConnectionOnCheckin(true);
+        	cpds.setTestConnectionOnCheckout(true); // If true, an operation will be performed
+        						// asynchronously at every connection checkin
+        						// to verify that the connection is valid.
+        	cpds.setPreferredTestQuery("SELECT 1;");
+        	cpds.setIdleConnectionTestPeriod(600); // If this is a number greater than 0,
+        						// c3p0 will test all idle,
+        						// pooled but unchecked-out connections, 
+        						// every this number of seconds.
+        	cpds.setMaxIdleTimeExcessConnections(120);
+        	cpds.setMaxIdleTime(600); // Seconds a Connection can remain pooled but 
+        				// unused before being discarded.
+        				// Zero means idle connections never expire.
+	        return cpds;
+    	}
+    /*public DataSource getDataSource() {
         DataSource dataSource = DataSourceBuilder
                 .create()
                 .username(username)
@@ -42,7 +69,7 @@ public class HibernateConfiguration {
                 .driverClassName(driverClassName)
                 .build();
         return dataSource;
-    }
+    }*/
 	
 	@Bean
 	public LocalSessionFactoryBean sessionFactory() throws URISyntaxException {
